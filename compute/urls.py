95ee54d954d54a50d28783daf5dc9c2b27caea8e
bougie <bougie@appartland.eu>
2013-10-25 23:01:52 +0200

    Suppression des routes obseletes

--- compute/urls.py
+++ compute/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'account.views.index', name='account_index'),
-	url(r'^login/$', 'account.views.login', name='account_login'),
-	url(r'^logout/$', 'account.views.logout', name='account_logout'),
-	url(r'^passowrd/$', 'account.views.password', name='account_password'),
-	url(r'^passowrd/([a-zA-Z0-9]{42})$', 'account.views.passwordValid', name='account_passwordValid'),
+	url(r'^$', 'account.views.index', name='compute_index'),
 )
