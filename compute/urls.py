b50a26d4ee45b1db031a010164cd6e1c1cc46df8
bougie <bougie@appartland.eu>
2013-10-26 10:47:41 +0200

    Ajout d'un compute

--- compute/urls.py
+++ compute/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 	url(r'^$', 'compute.views.index'),
+	url(r'^add/$', 'compute.views.add'),
 )
--- compute/views.py
+++ compute/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.utils import simplejson
 
 from compute.models import Compute
+from compute.forms import ComputeForm
 
 def index(request):
 	if request.method == 'GET':
@@ -17,5 +18,29 @@ def index(request):
 			"results": data
 		}
 		return HttpResponse(simplejson.dumps(response_data), content_type="application/json")
+	else:
+		return HttpResponse(status=501)
+
+def add(request):
+	if request.method == 'POST':
+		form = ComputeForm(request.POST)
+
+		if form.is_valid():
+			cte = Compute()
+
+			cte.name = form.cleaned_data['name']
+			cte.vcpu = form.cleaned_data['vcpu']
+			cte.memory = form.cleaned_data['memory']
+			cte.disk = form.cleaned_data['disk']
+			cte.ctype = form.cleaned_data['ctype']
+
+			try:
+				cte.save()
+
+				return HttpResponse(status=200)
+			except:
+				return HttpResponse(status=500)
+		else:
+			return HttpResponse(status=400)
 	else:
 		return HttpResponse(status=501)
